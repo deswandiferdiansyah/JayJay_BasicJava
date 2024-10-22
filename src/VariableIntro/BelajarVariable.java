01954eb0b3a7fe8920e31577239ded98526194da
Deswandi Ferdiansyah <deswandiferdiansyah@gmail.com>
2024-10-23 03:49:11 +0700

    BelajarBesicJava

---
+++ src/VariableIntro/BelajarVariable.java
@@ -0,0 +1,40 @@
+package VariableIntro;
+
+public class BelajarVariable {
+    public static void main(String[] args){
+
+        // Apa itu variable
+        // int sebuahAngka = 7;
+        // System.out.println(sebuahAngka);
+
+        // Variable declaration
+        int sebuahAngka;
+
+        // initialising variable
+        sebuahAngka = 10;
+        //System.out.println(sebuahAngka);
+
+        // multi variable declaration and initialization
+        int angkaPertama = 1, angkaKedua = 2, angkaKetiga = 3;
+
+        System.out.println(angkaPertama);
+        System.out.println(angkaKedua);
+        System.out.println(angkaKetiga);
+
+        // tipe data variable short, int, long, float. double. boolean, char, String
+
+        short angkaShort = 3278;
+        int angkaInteger = 4000;
+        long angkaLong = 999999999;
+
+        float angkaFloat = 32.1f;
+        double angkaDouble = 56.1394585;
+
+        boolean varBoolean = true;
+        boolean varBooleanFalse = false;
+
+        char varChar = 'a';
+
+        String varString = "Hello Java";
+    }
+}
